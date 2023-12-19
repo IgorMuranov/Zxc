6b23d9b29b062d8e00baba5e254d9f33dc8c7ebb
IgorMuranov <muranovigor37@gmail.com>
2023-12-20 00:15:54 +0300

    новый тест

--- src/test/java/testproject/Gaga.java
+++ src/test/java/testproject/Gaga.java
@@ -1,13 +1,35 @@
 package testproject;
 
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AggregatorTest {
 
     @Test
-    public void testInitialState() {
-        Aggregator agr = new Aggregator();
-        assertThat(agr.getSum()).isZero();
+    public void testAddValueAndGetSum() {
+        Aggregator aggregator = new Aggregator();
+
+        // Add some values
+        aggregator.addValue(5.0);
+        aggregator.addValue(10.5);
+        aggregator.addValue(3.5);
+
+        // Check if the sum is correct
+        assertEquals(5.0 + 10.5 + 3.5, aggregator.getSum(), 0.001);
+    }
+
+    @Test
+    public void testReset() {
+        Aggregator aggregator = new Aggregator();
+
+        // Add some values
+        aggregator.addValue(5.0);
+        aggregator.addValue(10.5);
+
+        // Reset the aggregator
+        aggregator.reset();
+
+        // Check if the sum is reset to zero
+        assertEquals(0.0, aggregator.getSum(), 0.001);
     }
 }
