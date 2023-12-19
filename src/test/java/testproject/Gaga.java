18c326a1ea9f5211d72ab9724a4ea74581f1e3af
IgorMuranov <muranovigor37@gmail.com>
2023-12-20 00:10:34 +0300

    Добавили тест

---
+++ src/test/java/testproject/Gaga.java
@@ -0,0 +1,13 @@
+package testproject;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class AggregatorTest {
+
+    @Test
+    public void testInitialState() {
+        Aggregator agr = new Aggregator();
+        assertThat(agr.getSum()).isZero();
+    }
+}
